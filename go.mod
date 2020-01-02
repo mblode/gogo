99ec74d464cbe42d9b5f21a2e28d4e21b734fcdb
Matthew Blode <m@blode.co>
2020-01-03 08:21:46 +1000

    GORM

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/mblode/gogo
 
 go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/jinzhu/gorm v1.9.11
+	github.com/joho/godotenv v1.3.0
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.11 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/sys v0.0.0-20200102141924-c96a22e43c9c // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,20 +1,131 @@
-package main
-
+package main // only need mysql OR sqlite
+// both are included here for reference
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
 )
 
+var db *gorm.DB
+
+type Person struct {
+	gorm.Model `json:"model"`
+	FirstName  string `json:"firstname"`
+	LastName   string `json:"lastname"`
+	City       string `json:"city"`
+}
+
+func handlerFunc(msg string) func(echo.Context) error {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, msg)
+	}
+}
+
+func getHome(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, world!")
+	}
+}
+
+func getPeople(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		var people []Person
+		db.Find(&people)
+		fmt.Println("{}", people)
+		return c.JSON(http.StatusOK, people)
+	}
+}
+
+func getPerson(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		id := c.Param("id")
+		var person Person
+		db.Where("id = ?", id).First(&person)
+		return c.JSON(http.StatusOK, person)
+	}
+}
+
+func createPerson(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		firstname := c.QueryParam("firstname")
+		lastname := c.QueryParam("lastname")
+		city := c.QueryParam("city")
+
+		db.Create(&Person{FirstName: firstname, LastName: lastname, City: city})
+		return c.String(http.StatusOK, firstname+" person successfully created")
+	}
+}
+
+func updatePerson(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		id := c.Param("id")
+		firstname := c.QueryParam("firstname")
+		lastname := c.QueryParam("lastname")
+		city := c.QueryParam("city")
+
+		var person Person
+		db.Where("id = ?", id).First(&person)
+		person.FirstName = firstname
+		person.LastName = lastname
+		person.City = city
+		db.Save(&person)
+		return c.String(http.StatusOK, lastname+" person successfully updated")
+	}
+}
+
+func deletePerson(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		var person Person
+		id := c.Param("id")
+		d := db.Where("id = ?", id).Delete(&person)
+		fmt.Println(d)
+		return c.JSON(http.StatusOK, id+" deleted")
+	}
+}
+
+func handleRequest(db *gorm.DB) {
+	e := echo.New()
+
+	e.GET("/", getHome(db))
+	e.GET("/people/", getPeople(db))
+	e.GET("/people/:id", getPerson(db))
+	e.POST("/people", createPerson(db))
+	e.PUT("/people/:id", updatePerson(db))
+	e.DELETE("/people/:id", deletePerson(db))
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func initialMigration(db *gorm.DB) {
+	db.AutoMigrate(&Person{})
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
-	})
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Path[len("/greet/"):]
-		fmt.Fprintf(w, "Hello %s\n", name)
-	})
+	dbServer := os.Getenv("DB_SERVER")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbDatabase := os.Getenv("DB_DATABASE")
+	dbPort := os.Getenv("DB_PORT")
 
-	fmt.Println("Server started at http://localhost:8080/")
-	http.ListenAndServe(":8080", nil)
+	db, err := gorm.Open("mysql", dbUser+":"+dbPassword+"@tcp("+dbServer+":"+dbPort+")/"+dbDatabase+"?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+
+	defer db.Close()
+	initialMigration(db)
+	handleRequest(db)
 }
